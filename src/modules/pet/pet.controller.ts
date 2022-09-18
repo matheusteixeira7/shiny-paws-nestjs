48ee53d86e924d0f1bd99fe5c1770bbb461181ba
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-18 16:25:58 -0300

    chore: add pet controller

---
+++ src/modules/pet/pet.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreatePetUseCase } from '../../@core/application/usecases/pet';
+import { CreatePetDto } from './dto/create-pet.dto';
+import { UpdatePetDto } from './dto/update-pet.dto';
+
+@Controller('pet')
+export class PetController {
+  constructor(private readonly createPetUseCase: CreatePetUseCase) {}
+
+  @Post()
+  create(@Param('id') id: string, @Body() createPetDto: CreatePetDto) {
+    return this.createPetUseCase.execute(id, createPetDto);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.petService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.petService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePetDto: UpdatePetDto) {
+  //   return this.petService.update(+id, updatePetDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.petService.remove(+id);
+  // }
+}
