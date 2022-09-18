6b59f02915897d2d8652fc2cd5ecf1c93ee79132
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-18 15:56:24 -0300

    chore: add pet usecase classes

---
+++ src/@core/application/usecases/pets/index.ts
@@ -0,0 +1,5 @@
+export * from './create-pet.usecase';
+export * from './findAll-pets.usecase';
+export * from './findOne-pet.usecase';
+export * from './remove-pet.usecase';
+export * from './update-pet.usecase';
