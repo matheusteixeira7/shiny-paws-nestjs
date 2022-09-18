44d68802ce3521c36342539e183992dec85f117c
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-18 14:51:03 -0300

    test: add find all customers

---
+++ src/@core/application/usecases/customer/__tests__/findAll-customers.usecase.spec.ts
@@ -0,0 +1,31 @@
+import { CreateCustomerUseCase, FindAllCustomersUseCase } from '..';
+import { InMemoryCustomerRepository } from '../../../../infra/db/in-memory';
+
+let customerRepository: InMemoryCustomerRepository;
+let createCustomerUseCase: CreateCustomerUseCase;
+let sut: FindAllCustomersUseCase;
+
+describe('List customers use case', () => {
+  beforeEach(() => {
+    customerRepository = new InMemoryCustomerRepository();
+    createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+    sut = new FindAllCustomersUseCase(customerRepository);
+  });
+  it('should be able to list customers', async () => {
+    await createCustomerUseCase.execute({
+      name: 'John Doe',
+      email: 'doe@example.com',
+      phone: '123456',
+      address: '123 Main St',
+    });
+
+    await createCustomerUseCase.execute({
+      name: 'Matheus Teixeira',
+      email: 'teixeira@example.com',
+      phone: '123456',
+      address: '123 Main St',
+    });
+
+    expect(await sut.execute()).toHaveLength(2);
+  });
+});
