f03d60e5b985b3920357d787872a86a30b566981
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-17 22:47:38 -0300

    chore: remove tsyringe

--- src/@core/infra/container/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from 'tsyringe';
-
-import { UserRepositoryInterface } from '../../domain/repositories/user.repository';
-import { InMemoryUserRepository } from '../db/in-memory/in-memory-user.repository';
-
-container.registerSingleton<UserRepositoryInterface>(
-  'InMemoryUsersRepository',
-  InMemoryUserRepository,
-);
