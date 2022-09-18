eb771c1c48ae624f85d294c2659367a04cbfba14
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-18 16:09:33 -0300

    refactor: change interface name and method name

--- src/@core/domain/repositories/pet.repository.ts
+++ src/@core/domain/repositories/pet.repository.ts
@@ -1,11 +1,11 @@
 import { Pet } from '../entities/pet.entity';
 
-export interface PetsRepository {
+export interface PetRepositoryInterface {
   findOneById(id: string): Promise<Pet | null>;
   findOneByName(name: string): Promise<Pet[] | null>;
   findOwnerById(ownerId: string): Promise<Pet[]>;
   create(pet: Pet): Promise<Pet>;
   update(pet: Pet): Promise<Pet>;
-  delete(id: string): Promise<void>;
+  remove(id: string): Promise<void>;
   findAll(): Promise<Pet[]>;
 }
