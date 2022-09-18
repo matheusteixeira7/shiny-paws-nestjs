ed895948ea6ee3237f85301ead9f8c39f3b25f7c
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-17 23:06:13 -0300

    refactor: update findOne user test

--- src/@core/application/usecases/user/__tests__/findOne-user.usecase.spec.ts
+++ src/@core/application/usecases/user/__tests__/findOne-user.usecase.spec.ts
@@ -1,13 +1,14 @@
-import { FindOneUserUseCase } from '..';
-import { User } from '../../../../domain/entities';
+import { CreateUserUseCase, FindOneUserUseCase } from '..';
 import { InMemoryUserRepository } from '../../../../infra/db/in-memory';
 
 let usersRepository: InMemoryUserRepository;
+let createUserUseCase: CreateUserUseCase;
 let sut: FindOneUserUseCase;
 
-describe('Get user use case', () => {
+describe('FindOneUserUseCase', () => {
   beforeEach(() => {
     usersRepository = new InMemoryUserRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
     sut = new FindOneUserUseCase(usersRepository);
   });
   it('should throw error if user do not exists', async () => {
@@ -19,13 +20,11 @@ describe('Get user use case', () => {
   });
 
   it('should be able to get an user', async () => {
-    const user = User.create({
-      name: 'Diego',
-      email: 'asd@email.com',
+    const user = await createUserUseCase.execute({
+      name: 'John Doe',
+      email: 'john@email.com',
       password: '123456',
     });
-
-    await usersRepository.create(user);
 
     const result = await sut.execute({
       id: user.id,
