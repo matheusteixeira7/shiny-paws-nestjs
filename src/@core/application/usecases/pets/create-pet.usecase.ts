08e2136464f58afeb87ecefd1606b332a34cde5e
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-18 16:10:14 -0300

    feat: add create pet feature

--- src/@core/application/usecases/pets/create-pet.usecase.ts
+++ src/@core/application/usecases/pets/create-pet.usecase.ts
@@ -1,1 +1,37 @@
-export class CreatePetUseCase {}
+import { Pet } from '../../../domain/entities';
+import {
+  CustomerRepositoryInterface,
+  PetRepositoryInterface,
+} from '../../../domain/repositories';
+
+type CreatePetDto = {
+  name: string;
+  specie: 'dog' | 'cat';
+  breed: string;
+};
+
+export class CreatePetUseCase {
+  constructor(
+    private petRepository: PetRepositoryInterface,
+    private customerRepository: CustomerRepositoryInterface,
+  ) {}
+
+  async execute(id: string, { name, specie, breed }: CreatePetDto) {
+    const customer = await this.customerRepository.findOneById(id);
+
+    if (!customer) {
+      throw new Error('Customer does not exist.');
+    }
+
+    const pet = Pet.create({
+      name,
+      specie,
+      breed,
+      owner: customer,
+    });
+
+    this.petRepository.create(pet);
+
+    return pet;
+  }
+}
