4911f204ec0c21341533a326b87f3c68b491f99b
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-19 15:34:02 -0300

    feat: add findAll

--- src/@core/application/usecases/pet/findAll-pets.usecase.ts
+++ src/@core/application/usecases/pet/findAll-pets.usecase.ts
@@ -1,1 +1,10 @@
-export class FindAllUseCases {}
+import { Pet } from '../../../domain/entities';
+import { PetRepositoryInterface } from '../../../domain/repositories';
+
+export class FindAllPetsUseCase {
+  constructor(private readonly petRepository: PetRepositoryInterface) {}
+
+  async execute(): Promise<Pet[]> {
+    return this.petRepository.findAll();
+  }
+}
