90f128a4b4cd2d0d502c04ad6ba6465a79d9c345
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-19 17:32:22 -0300

    feat: add remove pet

--- src/@core/application/usecases/pet/remove-pet.usecase.ts
+++ src/@core/application/usecases/pet/remove-pet.usecase.ts
@@ -1,1 +1,18 @@
-export class RemovePetUseCase {}
+import { Pet } from '../../../domain/entities';
+import { PetRepositoryInterface } from '../../../domain/repositories';
+
+export class RemovePetUseCase {
+  constructor(private petRepository: PetRepositoryInterface) {}
+
+  async execute(id: string): Promise<Pet> {
+    const pet = await this.petRepository.findOneById(id);
+
+    if (!pet) {
+      throw new Error('Pet not found');
+    }
+
+    await this.petRepository.remove(pet.id);
+
+    return pet;
+  }
+}
