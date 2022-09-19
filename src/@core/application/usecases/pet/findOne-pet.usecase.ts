3a66c2764c7175dfbeb74e67789eae8e8309fc03
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-19 17:24:37 -0300

    feat: add findOne pet

--- src/@core/application/usecases/pet/findOne-pet.usecase.ts
+++ src/@core/application/usecases/pet/findOne-pet.usecase.ts
@@ -1,1 +1,16 @@
-export class FindOnePetUseCase {}
+import { Pet } from '../../../domain/entities';
+import { PetRepositoryInterface } from '../../../domain/repositories';
+
+export class FindOnePetUseCase {
+  constructor(private petRepository: PetRepositoryInterface) {}
+
+  async execute(id: string): Promise<Pet> {
+    const pet = await this.petRepository.findOneById(id);
+
+    if (!pet) {
+      throw new Error('Pet not found');
+    }
+
+    return pet;
+  }
+}
