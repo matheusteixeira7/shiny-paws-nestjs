b0474229a0e86c305b93a4f210514dbe2d7e393f
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-19 19:06:47 -0300

    refactor: change class and method name

--- src/@core/domain/repositories/service.repository.ts
+++ src/@core/domain/repositories/service.repository.ts
@@ -1,11 +1,11 @@
 import { Service } from '../entities/service.entity';
 
-export interface ServicesRepository {
+export interface ServiceRepositoryInterface {
   findOneById(id: string): Promise<Service | null>;
   findManyById(ids: string[]): Promise<Service[]>;
   findOneByName(name: string): Promise<Service | null>;
   create(service: Service): Promise<Service>;
   update(service: Service): Promise<Service>;
   findAll(): Promise<Service[]>;
-  delete(id: string): Promise<void>;
+  remove(id: string): Promise<void>;
 }
