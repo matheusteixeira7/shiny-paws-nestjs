fd7741c6e3222e2f16091b641aba2a3e72294eef
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-19 21:40:46 -0300

    feat: add create service

--- src/@core/application/usecases/service/create-service.usecase.ts
+++ src/@core/application/usecases/service/create-service.usecase.ts
@@ -1,8 +1,23 @@
 import { Service } from '../../../domain/entities';
 import { ServiceRepositoryInterface } from '../../../domain/repositories';
 
+export type ServiceDto = {
+  name: string;
+  price: number;
+};
+
 export class CreateServiceUseCase {
   constructor(private readonly serviceRepository: ServiceRepositoryInterface) {}
 
-  async execute(service: Service): Promise<Service> {}
+  async execute({ name, price }: ServiceDto): Promise<Service> {
+    const nameExists = await this.serviceRepository.findOneByName(name);
+
+    if (nameExists) {
+      throw new Error('Service name already exists');
+    }
+
+    const service = Service.create({ name, price });
+
+    return this.serviceRepository.create(service);
+  }
 }
