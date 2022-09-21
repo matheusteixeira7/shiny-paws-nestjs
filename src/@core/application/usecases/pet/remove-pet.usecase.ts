028cd73cb74682d4d6df2e7270062aa916d3c8ef
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-09-21 11:21:33 -0300

    refactor: change return to void

--- src/@core/application/usecases/pet/remove-pet.usecase.ts
+++ src/@core/application/usecases/pet/remove-pet.usecase.ts
@@ -1,10 +1,9 @@
-import { Pet } from '../../../domain/entities';
 import { PetRepositoryInterface } from '../../../domain/repositories';
 
 export class RemovePetUseCase {
   constructor(private petRepository: PetRepositoryInterface) {}
 
-  async execute(id: string): Promise<Pet> {
+  async execute(id: string): Promise<void> {
     const pet = await this.petRepository.findOneById(id);
 
     if (!pet) {
@@ -12,7 +11,5 @@ export class RemovePetUseCase {
     }
 
     await this.petRepository.remove(pet.id);
-
-    return pet;
   }
 }
